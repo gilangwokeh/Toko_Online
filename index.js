052f992590cada3931c6cb6d9d811396a7832542
gilangwokeh <gilangwokeh12@gmail.com>
2023-05-24 14:04:54 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function toggleNav() {
-  const navLinks = document.getElementById("navLinks");
-  navLinks.classList.toggle("show-nav");
-}
+const navbarToggle = document.getElementById("navbar-toggle");
+const navbarMenu = document.getElementById("navbar-menu");
+const navbarClose = document.getElementById("navbar-close");
+
+navbarToggle.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+navbarClose.addEventListener("click", () => {
+  navbarMenu.classList.remove("open");
+});
 
 const carouselContainer = document.querySelector(".carousel-container");
 const carouselItems = document.querySelectorAll(".carousel-item");
@@ -28,3 +35,53 @@ function prevItem() {
 }
 
 setInterval(nextItem, 5000);
+
+const slider = document.querySelector(".slider");
+const sliderImages = document.querySelectorAll(".slider img");
+const totalSlides = sliderImages.length;
+let currentIndexSlider = 0;
+
+const prevButton = document.querySelector(".prev-btn");
+const nextButton = document.querySelector(".next-btn");
+const sliderButtons = document.querySelector(".slider-buttons");
+
+prevButton.addEventListener("click", () => {
+  prevSlide();
+});
+
+nextButton.addEventListener("click", () => {
+  nextSlide();
+});
+
+function showSlide(index) {
+  if (index < 0) {
+    index = totalSlides - 1;
+  } else if (index >= totalSlides) {
+    index = 0;
+  }
+
+  slider.style.transform = `translateX(-${index * 100}%)`;
+  currentIndexSlider = index;
+}
+
+function nextSlide() {
+  showSlide(currentIndexSlider + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndexSlider - 1);
+}
+
+showSlide(currentIndexSlider);
+
+const helpWhatsappBtn = document.getElementById("help-whatsapp-btn");
+const helpWhatsappPopup = document.getElementById("help-whatsapp-popup");
+const closePopup = document.getElementById("close-popup");
+
+helpWhatsappBtn.addEventListener("click", () => {
+  helpWhatsappPopup.style.display = "block";
+});
+
+closePopup.addEventListener("click", () => {
+  helpWhatsappPopup.style.display = "none";
+});
